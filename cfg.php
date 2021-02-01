7d2c2c91c2031a37e7c1ba3b1e630cc6b6382dcf
rico192 <ricoananda.9c26@gmail.com>
2021-02-01 14:32:06 +0700

    anu

---
+++ cfg.php
@@ -0,0 +1,5 @@
+<?php
+
+$link = "https://digitask.ru/notimer_fp/faucet.php?address=DCoMA8FxQZimE6PN5KAtu2trcz5B3qo8Uu&capcode=ac9d88763fcb824017b04bb990962a59%7Cf9919562a55027d57a922504af29190a%7Chttp%3A%2F%2Fback10.keycaptcha.com%2Fswfs%2Fckc%2Ff7203606f99cf5ba5652207941fcfa1b-%7C60180b9fb2626-4.0.0.001%7C1&wallet=¤cy=DOGE&key=a406f620e6366659864168beac8d2148";
+$ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36";
+$cookie = "valet_adress=DCoMA8FxQZimE6PN5KAtu2trcz5B3qo8Uu";
